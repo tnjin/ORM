6b91027b4635bc8e94348d9facf05752a4d23933
tnjin <t.jzhou@163.com>
2016-10-26 23:48:42 +0800

    增加一个单元测试——为了验证反射之后类是否相等，非功能测试

--- simrom/src/androidTest/java/com/tjz/pri/simrom/DataProcessorTest.java
+++ simrom/src/androidTest/java/com/tjz/pri/simrom/DataProcessorTest.java
@@ -2,11 +2,14 @@ package com.tjz.pri.simrom;
 
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
+import android.util.Log;
 
 import junit.framework.TestCase;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.lang.reflect.Method;
 
 /**
  * Created by tjz on 2016/10/25.
@@ -20,5 +23,30 @@ public class DataProcessorTest  extends TestCase{
         DataProcessor.init(InstrumentationRegistry.getContext());
         DataProcessor processor = DataProcessor.getInstance( );
         processor.createDb(entity.getClass());
+    }
+
+    @Test
+    public void testReflect(){
+        TestEntity entity = new TestEntity();
+        Method[] methods = entity.getClass().getDeclaredMethods();
+        for(Method m : methods){
+            StringBuilder sb = new StringBuilder();
+                sb.append("method:")
+                        .append(m.getName())
+                        .append("||param:");
+            Class [] paramTypes = m.getParameterTypes();
+            if(paramTypes == null){
+                sb.append("null");
+            }else if(paramTypes.length == 0){
+                sb.append("0");
+            }else{
+                for(Class clazz: paramTypes){
+                    sb.append(clazz.getSimpleName())
+                            .append("  ");
+                }
+            }
+            Log.i("SSSS",sb.toString());
+        }
+        Log.i("SSSS",""+(entity.getClass()==TestEntity.class));
     }
 }
--- simrom/src/androidTest/java/com/tjz/pri/simrom/TestEntity.java
+++ simrom/src/androidTest/java/com/tjz/pri/simrom/TestEntity.java
@@ -6,4 +6,20 @@ package com.tjz.pri.simrom;
 public class TestEntity extends BaseEntity {
     private int id;
     private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
